eaecb945480c2107d86afe26c19abe906abc7998
Luiz Sauer <luiz@macrokids.dev>
2026-07-05 22:07:34 -0300

    feat: Color Bezier connection wires dynamically based on Source Node Category type

--- src/MacroKids.NodeEditor/ViewModels/ConnectionViewModel.cs
+++ src/MacroKids.NodeEditor/ViewModels/ConnectionViewModel.cs
@@ -25,6 +25,24 @@ public sealed partial class ConnectionViewModel : ObservableObject
     public Point StartPoint => _sourceNode.GetOutputPinPoint(SourcePinId);
     public Point EndPoint => _targetNode.GetInputPinPoint(TargetPinId);
 
+    // Get color brush dynamically matching source node category
+    public System.Windows.Media.Brush StrokeBrush
+    {
+        get
+        {
+            if (IsAnimating) return System.Windows.Media.Brushes.Yellow;
+
+            return _sourceNode.Metadata.Category switch
+            {
+                NodeCategory.Keyboard => new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0x25, 0x63, 0xEB)),
+                NodeCategory.Mouse => new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0x16, 0xA3, 0x4A)),
+                NodeCategory.Loops => new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0x15, 0x80, 0x3D)),
+                NodeCategory.Events => new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0xDB, 0x27, 0x77)),
+                _ => new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0x7C, 0x3A, 0xED))
+            };
+        }
+    }
+
     public ConnectionViewModel(FlowConnection connection, NodeViewModel sourceNode, NodeViewModel targetNode)
     {
         ConnectionId = connection.Id;
